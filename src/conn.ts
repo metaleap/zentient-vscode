2750db969ca35e3358e9f11b0e8aa39b22dd1277
metaleap <metaleap@mailbox.org>
2017-03-19 21:03:07 +0100

    [autocommit] «I smell like a wet reducing clinic on Columbus Day!»

--- src/conn.ts
+++ src/conn.ts
@@ -4,17 +4,20 @@ import vswin = vs.window
 
 import * as z from './zentient'
 
-import * as icp from 'child_process'
+import * as cproc from 'child_process'
+import * as ipc from 'readline'
 
 
 
-let proc        :icp.ChildProcess | null    = null
+let proc        :cproc.ChildProcess         = null
+let procio      :ipc.ReadLine               = null
 let shutDown    :boolean                    = false
 let wasEverLive :boolean                    = false
 
 
 
 function dispose () {
+    if (procio) { procio.removeAllListeners()  ;  procio.close()  ;  procio = null }
     if (proc)  {  proc.kill()  ;  proc = null  }
 }
 
@@ -24,14 +27,19 @@ export function onExit () {
 
 export function onInit () {
     if (!vsproj.rootPath) {  z.out("Won't start `zentient` process because this window has no folder open.")  ;  return  }
-    if (!(proc = icp.spawn('zentient', [], { cwd: vsproj.rootPath }))) {  onFail()  ;  return  }
+    if (!(proc = cproc.spawn('zentient', [], { cwd: vsproj.rootPath }))) {  onFail()  ;  return  }
     proc.on('error', onError)   ;   proc.on('close', onExitOrClose)   ;   proc.on('exit', onExitOrClose)
     // if spawn failed, proc.pid seems to be `undefined` rather than a "bad int" like 0 or -1
     if (! (proc.pid && proc.stdin && proc.stdin.writable && proc.stdout && proc.stdout.readable && proc.stderr && proc.stderr.readable) ) {
         onFail()  ;  return
     }
+    if (! (procio = ipc.createInterface({ input: proc.stdout, output: proc.stdin, terminal: false, historySize: 0 }))) {
+        onFail()  ;  return
+    }
 
     wasEverLive = true
+    // procio.once('line', (bla)=> console.log(bla))
+    // proc.stdin.write("crikey mr\n", 'utf-8')
     z.out("`zentient` process started.")
 
     z.regCmd('zen.dbg.sendquery', onCmdSendQuery)
@@ -41,10 +49,14 @@ function onCmdSendQuery () {
     if (!proc) return thenDead()
     return vswin.showInputBox().then( (query)=> {
         if (!proc) return thenDead()
-        if (!proc.stdin.write(query, 'utf-8'))
-            return vswin.showErrorMessage("nodestreams want us to wait for 'drain'.. that's not on, up the buffer sizes stat!!")
-        return vswin.showInformationMessage("Now the readln..")
-    })
+        if (!query) return Promise.resolve<string>(null)
+        return new Promise<string>((resolve, reject)=> {
+            if (!proc.stdin.write(query+'\n', 'utf-8'))
+                reject("DRAIN")
+            else
+                procio.once('line', (resultline)=> resolve(resultline))
+        }).then(vswin.showInformationMessage, vswin.showErrorMessage)
+    }, console.log)
 }
 
 function onError (err :Error) {
