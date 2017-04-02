dff859f81c96c520a016924ce2c689eff2f8b852
Phil Schumann <metaleap@mailbox.org>
2017-04-02 19:15:49 +0200

    [autocommit] «Adult, n.: One old enough to know better.»

--- src/proj.ts
+++ src/proj.ts
@@ -5,13 +5,14 @@ import vswin = vs.window
 
 import * as node_path from 'path'
 
+import * as u from './util'
 import * as z from './zentient'
 import * as zconn from './conn'
 
 
 let vsreg:              boolean                 = false,
     vsdiag:             vs.DiagnosticCollection,
-    lastdiagreqtime:    number
+    lastdiagsettime:    number
 
 
 export function cfgTool (zid: string, cap: string) {
@@ -37,33 +38,35 @@ export function* onAlive () {
 function onFileEvent (file: vs.TextDocument, msg: string) {
     const langzid = z.fileLangZid(file)
     if (langzid) {
-        lastdiagreqtime = Date.now()
-        zconn.requestJson(msg + langzid + ':' + vsproj.asRelativePath(file.fileName))
-            .then(refreshDiag(lastdiagreqtime), z.outThrow)
+        const reqtime = Date.now()
+        return zconn.requestJson(msg + langzid + ':' + vsproj.asRelativePath(file.fileName))
+            .then(refreshDiag(reqtime), z.outThrow)
     }
+    return u.thenDont()
 }
 
 function onFileClose (file: vs.TextDocument) {
     //  for uri.scheme==='file', occurs only on real close, not on editor tab deactivate
-    onFileEvent(file, zconn.MSG_FILE_CLOSE)
+    return onFileEvent(file, zconn.MSG_FILE_CLOSE)
 }
 
 function onFileOpen (file: vs.TextDocument) {
     //  for uri.scheme==='file', occurs on open and whenever editor tab activate (on linux, BUT on windows only the former apparently WTF?)
-    onFileEvent(file, zconn.MSG_FILE_OPEN)
+    return onFileEvent(file, zconn.MSG_FILE_OPEN)
 }
 
 function onFileWrite (file: vs.TextDocument) {
-    onFileEvent(file, zconn.MSG_FILE_WRITE)
+    return onFileEvent(file, zconn.MSG_FILE_WRITE)
 }
 
 
 type RespDiag = { Code: string, Msg: string, PosLn: number, PosCol: number, Sev: number, Cat: string }
 type RespDiags = { [_relfilepath: string]: RespDiag[] }
 
-function refreshDiag (diagreqtime: number) {
+function refreshDiag (reqtime: number) {
     return (alldiagjsons: { [_zid: string]: RespDiags })=> {
-        if (vsdiag && diagreqtime <= lastdiagreqtime) { // ignore response if a newer diag req is pending or already there
+        let hasnewer = lastdiagsettime > reqtime
+        if (vsdiag && !hasnewer) { // ignore response if a newer diag req is pending or already there
             const all: [vs.Uri, vs.Diagnostic[]][] = []
             if (alldiagjsons)
                 for (const zid in alldiagjsons) {
@@ -79,8 +82,12 @@ function refreshDiag (diagreqtime: number) {
                             all.push([vs.Uri.file(node_path.join(vsproj.rootPath, relfilepath)), filediags])
                     }
                 }
-            vsdiag.clear()
-            vsdiag.set(all)
+            // hasnewer = lastdiagsettime > reqtime
+            // if (!hasnewer) {
+                lastdiagsettime = Date.now()
+                vsdiag.clear()
+                vsdiag.set(all)
+            // }
         }
     }
 }
