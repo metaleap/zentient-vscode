cf77fa42cf2e4dde2a67832b9a1b237f1977e15f
metaleap <metaleap@users.noreply.github.com>
2018-01-14 16:55:40 +0100

    [autopush] «Kiss me twice. I'm schizophrenic.»

--- src/vsc-dbg-launch.ts
+++ src/vsc-dbg-launch.ts
@@ -1,28 +1,30 @@
 import * as vs from 'vscode'
 import vswin = vs.window
 
+import * as z from './zentient'
 import * as zcfg from './vsc-settings'
+import * as zipc_pipeio from './ipc-pipe-io'
 import * as zvscmd from './vsc-commands'
+
+
+const dbgCfg = [{ type: 'zentient.dbg', name: '⟨ℤ⟩', request: 'launch' }]
+
 
 export function onActivate() {
     zvscmd.ensure('zentient.dbg.progInfo', dbgProgInfo)
-    vs.debug.registerDebugConfigurationProvider('zentient.dbg', { provideDebugConfigurations: dbgCfg })
-}
-
-function dbgCfg(_folder: vs.WorkspaceFolder, _cancel: vs.CancellationToken): vs.ProviderResult<vs.DebugConfiguration[]> {
-    return [{
-        type: 'zentient.dbg', name: '⟨ℤ⟩', request: 'launch'
-    }]
+    vs.debug.registerDebugConfigurationProvider('zentient.dbg', { provideDebugConfigurations: () => dbgCfg })
 }
 
 function dbgProgInfo() {
+    const last = zipc_pipeio.last
     const te = vswin.activeTextEditor
-    if (!te) return undefined
-    const td = te.document
-    if (!zcfg.languageIdOk(td)) return undefined
+    const td = (te && zcfg.languageIdOk(te.document)) ? te.document :
+        (last ? z.findTextFile(last.filePath) : undefined)
 
-    const args = [td.fileName]
-    if (td.isDirty || td.isUntitled)
+    if (!(td || last)) return {}
+
+    const args = [td ? td.fileName : last.filePath]
+    if (td && (td.isDirty || td.isUntitled))
         args.push(td.getText())
-    return { args: args, command: 'zentient-dbg-vsc-' + td.languageId }
+    return { args: args, command: 'zentient-dbg-vsc-' + (td ? td.languageId : last.langId) }
 }
