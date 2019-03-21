82c3a12d0af3eac1a5a0c4a6ce238045346ce573
metaleap <metaleap@users.noreply.github.com>
2019-03-21 17:43:54 +0100

    [autopush] «Even a blind pig stumbles upon a few acorns.»

--- src/z-extras.ts
+++ src/z-extras.ts
@@ -49,7 +49,7 @@ function onExtraPicked(te: vs.TextEditor, td: vs.TextDocument, runIpcId: zipc_re
     return (item: Item) => {
         if (!item) return
         const finalstep = (input = '') =>
-            vswin.withProgress<void>({ location: vs.ProgressLocation.Window, title: "Waiting for `" + item.label + "`…" },
+            vswin.withProgress<void>({ location: vs.ProgressLocation.Window, title: "Waiting for `" + item.label + "`..." },
                 (_progress) => zipc_req.forFile<void>(td, runIpcId, [item.id, input], onExtraResp, te, range)
             )
 
